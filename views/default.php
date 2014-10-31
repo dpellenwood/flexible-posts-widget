ca46ee4e5cf535d85b993d8883cd6d2525c0b333
David Paul Ellenwood <david@dpedesign.com>
2014-10-31 11:49:41 -0500

    Code formatting standardizations

--- views/default.php
+++ views/default.php
@@ -14,22 +14,22 @@ if ( !defined('ABSPATH') )
 
 echo $before_widget;
 
-if ( !empty($title) )
+if ( ! empty( $title ) )
 	echo $before_title . $title . $after_title;
 
-if( $flexible_posts->have_posts() ):
+if ( $flexible_posts->have_posts() ):
 ?>
 	<ul class="dpe-flexible-posts">
-	<?php while( $flexible_posts->have_posts() ) : $flexible_posts->the_post(); global $post; ?>
+	<?php while ( $flexible_posts->have_posts() ) : $flexible_posts->the_post(); global $post; ?>
 		<li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<a href="<?php echo the_permalink(); ?>">
 				<?php
-					if( $thumbnail == true ) {
+					if ( $thumbnail == true ) {
 						// If the post has a feature image, show it
-						if( has_post_thumbnail() ) {
+						if ( has_post_thumbnail() ) {
 							the_post_thumbnail( $thumbsize );
 						// Else if the post has a mime type that starts with "image/" then show the image directly.
-						} elseif( 'image/' == substr( $post->post_mime_type, 0, 6 ) ) {
+						} elseif ( 'image/' == substr( $post->post_mime_type, 0, 6 ) ) {
 							echo wp_get_attachment_image( $post->ID, $thumbsize );
 						}
 					}
